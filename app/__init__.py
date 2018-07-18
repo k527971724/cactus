6d6b14580330c70b7a3ca309ed59918be12bf297
kira <k527971724@.gmail.com>
2018-07-18 16:48:03 +0800

    注册了api蓝本

--- app/__init__.py
+++ app/__init__.py
@@ -17,4 +17,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint)
+
     return app
